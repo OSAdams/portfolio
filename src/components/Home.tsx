6cea89e50f690dbcde7453ab5b1f25efb4f4ae43
Owen Adams <falcocodes@osastack.dev>
2024-09-08 12:15:39 -0400

    import logo svgs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,19 @@
+import SvgLogo from '../assets/whiteosastack.svg';
+
 export default function Home() {
 	return (
 		<>
 			<div className="navbar bg-base-100">
-				<a href="https://www.osastack.dev" className="btn btn-ghost text-xl">
-					www.osastack.dev
-				</a>
+				<div className="flex-none">
+					<button className="btn btn-square btn-ghost">
+						<img src={SvgLogo} alt="https://www.osastack.dev" />
+					</button>
+				</div>
+				<div className="flex-1">
+					<h1 className="btn btn-ghost text-xl">
+						Welcome to www.osastack.dev!
+					</h1>
+				</div>
 			</div>
 		</>
 	);
